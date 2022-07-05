a620b51038f615d945b965e62ef3ddb060c04009
kienbuivan <kienbv.hust@gmail.com>
2022-07-05 16:10:57 +0700

    Fix Images not displayed. 1. Config working directory 2. change path folder with macOs. ( remove "/") in MvcConfig.java

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MvcConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MvcConfig.java
@@ -1,11 +1,11 @@
 package com.shopme.admin;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -16,9 +16,9 @@ public class MvcConfig implements WebMvcConfigurer {
 		Path userPhotosDir = Paths.get(dirName);
 		
 		String userPhotosPath = userPhotosDir.toFile().getAbsolutePath();
-		
+
 		registry.addResourceHandler("/" + dirName + "/**")
-			.addResourceLocations("file:/" + userPhotosPath + "/");
+				.addResourceLocations("file:" + userPhotosPath + "/");// MacOs  not file:/
 	}
 
 }
