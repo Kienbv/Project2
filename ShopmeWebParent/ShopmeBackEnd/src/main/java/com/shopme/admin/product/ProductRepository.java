58762f25f8cdbd620482c5c8900100f7da3239b8
kienbuivan <kienbv.hust@gmail.com>
2022-07-08 10:54:55 +0700

    Revert "Test ProductRepositoryTests.java and change List.of() -> Arrays.listOf()"
    This reverts commit c1e726d31c40ccabbc679b7033363e5077e9c4ff.

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shopme.admin.product;
-
-import com.shopme.common.entity.Product;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
-}
